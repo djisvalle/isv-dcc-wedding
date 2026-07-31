b3442e4e9d0cf9b7c16376dfefd36cb245a16c37
Israel Valle <israelvalle48@gmail.com>
2026-07-31 13:40:23 +0800

    feat: add TanStack Query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import {QueryClientProvider} from '@tanstack/react-query';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './lib/AuthContext';
+import { queryClient } from './lib/queryClient';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
